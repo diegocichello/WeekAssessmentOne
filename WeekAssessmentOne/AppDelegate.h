a04a197a393b70fbd6d7cc788376f1813b99cd31
diegocichello <diegocichello@Diegos-MacBook-Pro.local>
2015-01-09 09:16:01 -0800

    Initial Commit

---
+++ WeekAssessmentOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekAssessmentOne
+//
+//  Created by Diego Cichello on 1/9/15.
+//  Copyright (c) 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
